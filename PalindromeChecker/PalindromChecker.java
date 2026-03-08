54ad1b48fe02a03f42b84118c797e05d7942f9c5
Sreeram Venugopal <135961017+1234Sree0707@users.noreply.github.com>
2026-03-08 21:12:46 +0530

    Displays welcome to palindrome checker

---
+++ PalindromeChecker/PalindromChecker.java
@@ -0,0 +1,27 @@
+package com.seveneleven.palindromecheckerapp.main;
+
+/**
+ * 
+ * MAIN CLASS
+ * 
+ * 
+ * Use Case 1 :Application Entry and welcome message
+ * 
+ * Description: This class represents the entry point of the Palindrome checker management system
+ * 
+ * The goal is to establish a clear startup flow.
+ * 
+ * - Start execution from the main()
+ * - Displays a welcome message
+ * - Shows application version
+ * 
+ * @author Developer
+ * @version 1.0
+ */
+public class UseCaseOnePalindromeCheckerApp {
+	public static void main(String args[]) {
+		System.out.println("Welcome to Palindrome Checker Management System");
+		System.out.println("Version : 1.0");
+		System.out.println("System initialized successfully");
+	}
+}
